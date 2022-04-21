52c43cd37cd5f1ae9884155a9d38949dddefa9a6
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-21 19:58:57 +0200

    tested render and action of button component

--- public/js/component/Button/Button.test.js
+++ public/js/component/Button/Button.test.js
@@ -1,4 +1,3 @@
-import { TestWatcher } from "jest";
 import Button from "./Button";
 
 describe("Given, a button component", () => {
@@ -10,13 +9,25 @@ describe("Given, a button component", () => {
   });
 
   describe("When, instanteishon given a parent", () => {
-    TestWatcher("Then, it should render a button inside of it's parent", () => {
+    test("Then, it should render a button inside of it's parent", () => {
       const button = new Button(container);
       const expectedContainerClass = "testDiv";
 
       const parentClass = button.element.parentNode.className;
 
       expect(parentClass).toBe(expectedContainerClass);
+    });
+  });
+
+  describe("When, instanteishon given a class 'icon--delete' an action and the users clicks on it", () => {
+    test("Then, it should call the received action", () => {
+      const buttonAction = jest.fn();
+      new Button(container, buttonAction);
+
+      const buttonClick = container.querySelector(".icon--delete");
+      buttonClick.click();
+
+      expect(buttonAction).toHaveBeenCalledTimes(1);
     });
   });
 });
